58c213d448f140e36419de9a59ac306f30cf731d
Dennis Schwerdel <schwerdel@googlemail.com>
2015-11-02 21:35:33 +0100

    Tests for stats and sending

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,24 @@
 use super::*;
+use super::stats::Stats;
 
-#[cfg(feature = "nightly")]
 use std::time::Duration;
+
+#[test]
+fn stats() {
+    let mut stats = Stats::new(Duration::from_millis(100));
+    assert_eq!(stats.total(), 0);
+    assert_eq!(stats.rate(), 0.0);
+    let idle1 = stats.idle_time();
+    assert!(idle1 < Duration::from_millis(100));
+    assert!(idle1 > Duration::from_millis(0));
+    stats.update(100);
+    assert_eq!(stats.total(), 100);
+    let idle2 = stats.idle_time();
+    assert!(idle2 < Duration::from_millis(100));
+    assert!(idle2 > Duration::from_millis(0));
+    assert!(idle1 != idle2);
+    assert!(stats.rate() > 0.0);
+}
 
 #[test]
 fn node_create() {
@@ -33,4 +50,31 @@ fn node_connect() {
     }
     assert!(server.is_connected(&1));
     assert_eq!(client.receive(), Event::Connected(0));
+}
+
+#[test]
+fn node_send_self() {
+    let node = Node::new(0, 0);
+    assert!(node.listen("localhost:0").is_ok());
+    assert!(node.send(&0, &42).is_ok());
+    assert_eq!(node.receive(), Event::Message(0, 42));
+    assert!(!node.is_connected(&0));
+}
+
+#[test]
+fn node_send_remote() {
+    let server = Node::<u64, u64, u64>::new(0, 0);
+    assert!(server.listen("localhost:0").is_ok());
+    let client = Node::new(1, 1);
+    assert!(client.listen("localhost:0").is_ok());
+    assert!(client.connect(server.addresses()[0]).is_ok());
+    if let Event::ConnectionRequest(req) = server.receive() {
+        req.accept();
+    } else {
+        assert!(false);
+    }
+    assert_eq!(client.receive(), Event::Connected(0));
+    assert_eq!(server.receive(), Event::Connected(1));
+    assert!(client.send(&0, &42).is_ok());
+    assert_eq!(server.receive(), Event::Message(1, 42));
 }
